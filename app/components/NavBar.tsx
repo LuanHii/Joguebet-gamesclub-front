a0c113114351763ac3310bee9c1cd56219e0281a
dudsmeac <carvalhodudaandrade@gmail.com>
2025-08-03 13:14:23 -0300

    add theme toggle

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import ThemeToggle from "./ThemeToggle"
 
 export function NavBar() {
-    return (
-        <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+  return (
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-14 max-w-screen-2xl items-center justify-between p-4">
         <Link href="/" className="flex items-center space-x-2">
           <span className="font-bold text-lg">Joguebet</span>
@@ -11,8 +12,9 @@ export function NavBar() {
           <Link href="/" className="transition-colors hover:text-foreground/80 text-foreground">Já Jogados</Link>
           <Link href="/adicionar" className="transition-colors hover:text-foreground/80 text-foreground">Adicionar Jogo</Link>
           <Link href="/sortear" className="transition-colors hover:text-foreground/80 text-foreground">Sortear Jogo</Link>
+        <ThemeToggle />
         </nav>
       </div>
     </header>
-    )
+  )
 }
